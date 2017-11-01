5f927fcdafabde3d6165fccbe23d5b6370a24ffe
santtone <sami.anttonen@trimble.com>
2017-11-01 12:10:58 +0200

    Created contact folder/file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatInputModule, MatListModule} from '@angular/material';
+import { ContactListComponent } from './contact/contact-list/contact-list.component';
+import { ContactListItemComponent } from './contact/contact-list/contact-list-item/contact-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactListItemComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatListModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
