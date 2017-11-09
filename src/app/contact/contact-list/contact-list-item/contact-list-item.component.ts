017ae9764b3c524df30fcd914cc521b952417151
santtone <sami.anttonen@trimble.com>
2017-11-09 23:54:10 +0200

    Implemented select event of the contact list item

--- src/app/contact/contact-list/contact-list-item/contact-list-item.component.ts
+++ src/app/contact/contact-list/contact-list-item/contact-list-item.component.ts
@@ -1,6 +1,7 @@
-import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, EventEmitter, HostListener, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {Contact} from '../../contact';
 import {MatMenuTrigger} from '@angular/material';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'ca-contact-list-item',
@@ -10,11 +11,21 @@ import {MatMenuTrigger} from '@angular/material';
 export class ContactListItemComponent implements OnInit {
 
   @Input() contact: Contact;
+  @Output() select: EventEmitter<Contact>;
   @ViewChild(MatMenuTrigger) menuTrigger: MatMenuTrigger;
   menuIsVisible: boolean;
 
+  @HostListener('click', ['$event'])
+  public onClick(event) {
+    const buttonClicked = _.find(event.path, {'tagName': 'BUTTON'});
+    if (!buttonClicked) {
+      this.select.emit(this.contact);
+    }
+  }
+
   constructor() {
     this.menuIsVisible = false;
+    this.select = new EventEmitter();
   }
 
   ngOnInit() {
