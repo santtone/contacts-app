408825348765435601530eaf5ad5cca109968c40
santtone <sami.anttonen@trimble.com>
2017-11-12 22:21:54 +0200

    Implemented edit and add of contacts

--- src/app/contact/contact-details/contact-details.component.ts
+++ src/app/contact/contact-details/contact-details.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Location} from '@angular/common';
 import {Contact} from '../contact';
 import {ActivatedRoute} from '@angular/router';
 import {ContactService} from '../services/contact.service';
@@ -11,17 +12,24 @@ import {ContactService} from '../services/contact.service';
 export class ContactDetailsComponent implements OnInit {
 
   contact: Contact;
+  isNewContact: boolean;
 
-  constructor(private route: ActivatedRoute, private contactService: ContactService) {
+  constructor(private route: ActivatedRoute, private contactService: ContactService, private location: Location) {
     this.contact = new Contact();
+    this.isNewContact = true;
   }
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.contact = this.contactService.findContactById(parseInt(id, 10));
+      this.isNewContact = false;
     }
+  }
 
+  save() {
+    this.contactService.saveContact(this.contact);
+    this.location.back();
   }
 
 }
