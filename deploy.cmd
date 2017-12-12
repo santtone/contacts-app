a7f5bdbc00b5f72506ee257e5867ec2db558fd4a
santtone <sami.anttonen@trimble.com>
2017-12-12 13:09:27 +0200

    Changed deployment script

--- deploy.cmd
+++ deploy.cmd
@@ -113,7 +113,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd "!NODE_EXE!" ./node_modules/@angular/cli/bin/%ANGULAR_BUILD_COMMAND%
   echo Angular webapp built using command: %ANGULAR_BUILD_COMMAND%
-  call :ExecuteCmd cp "%DEPLOYMENT_SOURCE%"/web.config "%DEPLOYMENT_TARGET%"/dist/
+  call :ExecuteCmd cp "%DEPLOYMENT_TARGET%"/web.config "%DEPLOYMENT_TARGET%"/dist/
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
